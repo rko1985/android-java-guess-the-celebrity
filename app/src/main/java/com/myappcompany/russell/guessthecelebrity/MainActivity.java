fc5dd1c838605841383ee04c2a5cdad2638275bc
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-18 10:47:00 -1000

    download image and randomly choose image for imageView

--- app/src/main/java/com/myappcompany/russell/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/guessthecelebrity/MainActivity.java
@@ -1,18 +1,46 @@
 package com.myappcompany.russell.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int choosenCeleb = 0;
+    ImageView imageView;
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+            } catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
     public class DownloadTask extends AsyncTask <String, Void, String>{
 
@@ -48,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
+
         DownloadTask task = new DownloadTask();
         String result = null;
 
@@ -62,16 +92,21 @@ public class MainActivity extends AppCompatActivity {
             Matcher m = p.matcher(splitResult[0]);
 
             while(m.find()){
-                System.out.println(m.group(1));
+                celebURLs.add(m.group(1));
             }
 
             p = Pattern.compile("alt=\"(.*?)\"");
             m = p.matcher(splitResult[0]);
 
             while(m.find()){
-                System.out.println(m.group(1));
+                celebNames.add(m.group(1));
             }
 
+            Random rand = new Random();
+            choosenCeleb = rand.nextInt(celebURLs.size());
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap celebImage = imageTask.execute(celebURLs.get(choosenCeleb)).get();
+            imageView.setImageBitmap(celebImage);
 
         } catch(Exception e){
             e.printStackTrace();
